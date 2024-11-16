f37a66da84618e07422bf6c61fda553e452b3a73
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-11-16 16:26:01 +0000

    Emplementado listatodos

--- src/main/java/br/com/springboot/curso_dev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_dev_treinamento/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.springboot.curso_dev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +30,6 @@ public class GreetingsController {
         return "Curso Spring Boot API: " + name + "!";
     }
     
-    
-    //Configuraçoões Importante para o projeto
     @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET )
     @ResponseStatus(HttpStatus.OK)
     public String retornaOlaMundo(@PathVariable String nome) {
@@ -38,6 +41,13 @@ public class GreetingsController {
     	
     	return "Ola mundo: " + nome;
 
+    }
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> listaUsuario() {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        return new ResponseEntity<>(usuarios, HttpStatus.OK);
     }
 	
     
