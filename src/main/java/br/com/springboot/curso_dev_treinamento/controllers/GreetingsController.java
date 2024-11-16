8a41a50da4693457a05edadb3994691707f54296
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-11-16 17:57:13 +0000

    empl atualizar

--- src/main/java/br/com/springboot/curso_dev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_dev_treinamento/controllers/GreetingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -82,6 +83,19 @@ public class GreetingsController {
     Usuario usuario = usuarioRepository.findById(iduser).get();
     	
     	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    	
+    }
+    
+    @PutMapping(value = "atualizar")
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){
+    	if(usuario.getId()== null) {
+    		return new ResponseEntity<String>("Id não foi informado para atualização.", HttpStatus.OK);
+    	}
+    	
+    Usuario user = usuarioRepository.saveAndFlush(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.OK);
     	
     }
 	
